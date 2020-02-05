b7760f6c5c4ee29c3671063d3a8f575cc6e5be47
Lucas MOULIN <lmoulin@e2r1p7.42.fr>
2020-02-05 18:34:05 +0100

    write

--- main.c
+++ main.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
-
-//int		ft_strlen(char *s);
-char	*ft_strcpy(char *dst, const char *src);
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+//size_t		ft_strlen(char *s);
+//char	*ft_strcpy(char *dst, const char *src);
+ssize_t		ft_write(int fd, char *msg, int len);
 
 int main(int argc, char *argv[])
 {
-	char	dst[20];
+	char	*dst;
 	char	*src = argv[1];
+	char	*dst2;
+	char	*src2 = argv[1];
+	int		fd = open("test.txt", O_RDWR);
+	int		fd2 = open("test2.txt", O_RDWR);
 
-	dst[19] = '\0';
+	if (!(dst = malloc(sizeof(char) * 20)))
+		return (-1);
+	if (!(dst2 = malloc(sizeof(char) * 20)))
+		return (-1);
+	dst = argv[2];
+	dst2 = argv[2];
+
+//	dst[19] = '\0';
+//	dst2[19] = '\0';
 /*
 	printf("before argv1 = %s\n", argv[1]);
-	printf("%d\n", ft_strlen(argv[1]));
+	printf("%zu\n", ft_strlen(argv[1]));
+	printf("%zu\n", strlen(argv[1]));
 	printf("after argv1 = %s\n", argv[1]);
 */
-	printf("before dest = %s, src = %s\n", dst, src);
+/*	printf("before dest = %s, src = %s\n", dst, src);
 	ft_strcpy(dst, (const char *)src);
-	printf("before dest = %s, src = %s\n", dst, src);
+	printf("my dest = %s, src = %s\n", dst, src);
+	strcpy(dst2, (const char *)src2);
+	printf("true dest = %s, src = %s\n", dst, src);
+*/
+	printf("fd = 45 good ret = %zd\n", write(-45, "Salut\n", 0));
+	printf("fd = 45 My ret = %zd\n", ft_write(-45, "Salut\n", 0));
+
+	printf("fd = -12 good ret = %zd\n", write(0, "Salut\n", -6));
+	printf("fd = -12 My ret = %zd\n", ft_write(0, "Salut\n", -6));
+
+	printf("fd = 1 good ret = %zd\n", write(2, "Salut les amis\n", 20));
+	printf("fd = 1 My ret = %zd\n", ft_write(2, "Salut les amis\n", 20));
 	return 0;
 }
